30b20d402c2dd168ee0881018af8b5a4423a851c
Ricardo Branco <rbranco@suse.de>
2026-07-19 12:41:34 +0200

    Sanitize comm to avoid ANSI sequences, etc

--- common.bpf.h
+++ common.bpf.h
@@ -6,12 +6,22 @@ static __always_inline void log_denied(const char *prog, uid_t uid)
 {
 #if LOGGING
 	char comm[16];
+	unsigned int i;
+	unsigned char c;
 
 	bpf_get_current_comm(&comm, sizeof(comm));
-	/* comm is attacker-controlled (prctl(PR_SET_NAME), argv[0]) and could
-	 * contain text like "x uid=0" to spoof a naive log parser -- keep it
-	 * last so it can't clobber a field that actually matters.
+	/*
+	 * comm is attacker-controlled and could control characters and ANSI
+	 * escapes. Blank out the C0 control range and DEL, not anything >=
+	 * 0x80 so legitimate UTF-8 names survive. Comparing as unsigned char
+	 * matters since plain (signed) char turns bytes above 0x80 negative.
+	 * Keep it last in the format string so it can't spoof other fields.
 	 */
+	for (i = 0; i < sizeof(comm) - 1 && comm[i]; i++) {
+		c = comm[i];
+		if (c < 0x20 || c == 0x7f)
+			comm[i] = '?';
+	}
 	bpf_printk("%s: denied pid=%d uid=%d comm=%s",
 		   prog, bpf_get_current_pid_tgid() >> 32, uid, comm);
 #endif
